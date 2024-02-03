1111e189e7c231c98749e5776eff6456d7e941d0
Pavel Anpin <pavel@apes.ge>
2024-02-03 14:23:06 +0100

    refactored no prism config

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,27 +3,12 @@
 public partial class App : Application
 {
 
-	static NavigationPage GetPage()
-	{
-		var nav = new NavigationPage(new Splash());
-		
-		Task.Run(async () =>
-		{
-					await Task.Delay(1000);
-					var model = new PageModel();
-					var page = new MainPage() { BindingContext = model };
-					// await nav
-					await Current!.Dispatcher.DispatchAsync( async () => await nav.Navigation.PushAsync(page));
-					await Task.Delay(1000);
-					await model.RefreshCommand.ExecuteAsync(default);
-		});
-		return nav;
-	}
+	
 	public App()
 	{
 		InitializeComponent();
 		#if __NO_PRISM__
-		MainPage = GetPage();
+		MainPage = new NavigationPage(new AppShell_MAUI());
 		#endif 
 	}
 }
---
+++ AppShell_MAUI.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ios_layout;
+
+public partial class AppShell_MAUI : ContentPage
+{
+	public AppShell_MAUI()
+	{
+		InitializeComponent();
+	}
+	private async void Button_OnClicked(object? sender, EventArgs e)
+	{
+			
+		var model = new PageModel();
+		var page = new MainPage() { BindingContext = model };
+		// await nav
+		await this.Navigation.PushAsync(page);
+		// await Current!.Dispatcher.DispatchAsync( async () => await nav.Navigation.PushAsync(page));
+		// await Task.Delay(1000);
+		await model.RefreshCommand.ExecuteAsync(default);
+	}
+}
